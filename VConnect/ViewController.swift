6236e642a77d5e35f4f78eb62ca2926243cf2227
Donkemezuo <raymonddonkemezuo.org>
2019-02-07 19:04:53 -0500

    First push

---
+++ VConnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VConnect
+//
+//  Created by Donkemezuo Raymond Tariladou on 2/7/19.
+//  Copyright © 2019 EnProTech Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
